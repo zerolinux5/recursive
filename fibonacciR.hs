70dbe793f4b0b0666824569e1175c8406d5a367d
Jesus Magana <maganazero5@gmail.com>
2014-01-29 20:27:41 -0800

    added own reversal function

--- fibonacciR.hs
+++ fibonacciR.hs
@@ -1,7 +1,13 @@
+--fibonacci sequence via recursion, 2 line vs multiline in c++
 fibonacciR x
 	| x <= 2 = 1
 	| otherwise = fibonacciR (x-1) + fibonacciR(x-2)
 
+--built in i'm sure but here is personal version
 factorialR x 
 	| x == 1 = 1
 	| otherwise = x * factorialR (x-1)
+
+--reverse a string
+reverseR (x:xs) = reverseR (xs) ++ [x]  
+reverseR x = x
